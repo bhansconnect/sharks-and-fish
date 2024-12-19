2f2420536ae3efb32d0317abe51c09bb0177ff6d
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2024-12-19 10:47:05 -0800

    correctly add config and add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sharks and Fish
+
+Assuming I ever finish this, it will hopefully be a silly sim.
+It pits hunter sharks that use a DQN to learn against
+a population of fish that reproduce using genetic algorithms.
+What will learn faster? What will be best in the end?
---
+++ src/config.rs
@@ -0,0 +1,24 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, Clone, Copy)]
+pub struct Config {
+    pub sim: Sim,
+    pub target_fps: u32,
+    pub sharks: Sharks,
+}
+
+#[derive(Deserialize, Debug, Clone, Copy)]
+pub struct Sim {
+    pub width: f32,
+    pub height: f32,
+}
+
+#[derive(Deserialize, Debug, Clone, Copy)]
+pub struct Sharks {
+    pub count: u32,
+    pub linear_damping: f32,
+    pub angular_damping: f32,
+    pub max_force: f32,
+    pub max_reverse_force: f32,
+    pub max_torque: f32,
+}
